c1d4429da2aa335637510529292b73e1d9e4c912
19802162868 <15764226684@163.com>
2019-09-02 08:25:07 +0800

    1.0 version modao

--- modao/src/App.vue
+++ modao/src/App.vue
@@ -1,28 +1,74 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view></router-view>
+    <router-view name="footer"></router-view>
   </div>
 </template>
+<script>
+export default {
+  
+}
+</script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import '@/lib/reset.scss';
+html, body, #app {
+  @include rect(100%, 100%);
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+#app {
+  // font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  // -webkit-font-smoothing: antialiased;
+  // -moz-osx-font-smoothing: grayscale;
+  // text-align: center;
+  // color: #2c3e50;
+  @include flexbox();
+  @include flex-direction(column);
+  .container {
+    // -webkit-box-flex: 1; -ms-flex: 1; flex: 1; width: .1px;
+    @include flex();
+    @include rect(100%, auto); // 因为flex(),垂直方向默认宽度为.1px
+    @include flexbox();
+    @include flex-direction(column);
+    .header {
+      @include rect(100%,.44rem);
+      @include background-color(rgb(59, 89, 243));
+      @include font-weight(500);
+      @include flexbox();
+      @include justify-content(); // 水平居中
+      @include align-items(); // 垂直居中
+      @include color(#fff);
+      @include font-weight(600);
+      @include font-size(16px);
+    }
+    .content {
+      @include flex(); // 因为flex()，垂直方向垂直宽度为1px
+      @include rect(100%,100%);
+      // @include margin(.15rem);
+    }
+  }
+  .footer {
+    @include rect(100%,.55rem);
+    @include background-color(#e0e0e0);
+    ul{
+      @include rect(100%, 100%);
+      @include flexbox();
+      li{
+        @include flex();
+        @include flexbox();
+        @include flex-direction(column);
+        @include justify-content();
+        @include align-items();
+        &.router-link-active{
+          @include color(rgb(55, 16, 213));
+          @include font-weight(800);
+      }
+      }
+      span{
+        @include font-size(.24rem);
+      }
+      p{
+        @include font-size(.12rem);
+      }
     }
   }
 }
---
+++ modao/src/views/home/index.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="container">
+    <header class="header">首页</header>
+    <div class="content">
+      <div class="banner">
+        <van-swipe :autoplay="3000">
+          <van-swipe-item v-for="(item, index) in bannerlist" :key="index">
+            <img :src="item" alt="">
+            <!-- <img v-lazy="image" /> -->
+          </van-swipe-item>
+        </van-swipe>
+      </div>
+      <!-- <van-search v-model="value" placeholder="请输入搜索关键词" show-action shape="round" @search="onSearch">
+        <div slot="action" @click="onSearch">搜索</div>
+      </van-search> -->
+      <van-search placeholder="请输入搜索关键词" v-model="value" />
+      <div class="nav">
+        <span class="company">看公司</span>
+        <span class="rltship">人脉</span>
+        <span class="interview">面试</span>
+        <span class="question">问答</span>
+      </div>
+      <ul class="recommend">
+        <li>
+          <div class="title">
+            <div class="title_l">推荐</div>
+            <div class="title_r">换一批</div>
+          </div>
+          <div class="cont">
+            <div class="contInfo"></div>
+            <div class="contImg"></div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+import Vue from 'vue'
+import { Swipe, SwipeItem, Search } from 'vant'
+import axios from 'axios'
+
+Vue.use(Swipe).use(SwipeItem)
+Vue.use(Search)
+
+export default {
+  data () {
+    return {
+      bannerlist: []
+    }
+  },
+  mounted () {
+    axios.get('https://www.daxunxun.com/banner').then(res => { // 请求数据成功
+    /**
+     * ['/images/1.jpg']
+     * ['https://www.daxunxun.com/images/1.jpg']
+     */
+      let arr = []
+      res.data.map(item => {
+        arr.push('https://www.daxunxun.com' + item)
+      })
+      // console.log(arr)
+      this.bannerlist = arr
+    }).catch(err => { // 请求失败
+      console.log(err)
+    })
+  }
+}
+</script>
+<style lang="scss">
+@import '@/lib/reset.scss';
+.container{
+  .content{
+    .banner { width: 100%; height: 1.8rem; overflow: hidden;
+      .van-swipe { width: 100%; height: 100%;
+        img { width: 100%; border: 1px solid #ccc; }
+      }
+    }
+    .van-search__content{
+      background: rgb(236, 236, 236);
+      border-radius: .2rem;
+    }
+    .nav{
+      display: flex;
+      justify-content: space-around;
+      padding: 0 .08rem;
+      span{ width: .5rem; height: .5rem; border-radius: 50%; background-color: rgb(143, 171, 255); 
+      display: flex; align-items: center; justify-content: center; }
+    }
+    .recommend{
+      li{
+        .title{}
+        .cont{
+          .contInfo{}
+          .contImg{}
+        }
+      }
+    }
+  }
+}
+</style>
